b4d9570d04e4c20cc62c8014daf48cd39221f8f5
Nyurgun <burtsev.ns@gmail.com>
2022-03-08 19:24:35 +0900

    Add new cells function

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
 
     @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
@@ -19,11 +21,32 @@ class NewEmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
+        updateSaveButtonState()
     }
+    
+    
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
     //Action для всех текстовых полей
     @IBAction func textChange(_ sender: UITextField) {
+        updateSaveButtonState()
     }
-       
+    //функция срабатывает при перемещении со второго экрана на первый
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+    }
 
 }
